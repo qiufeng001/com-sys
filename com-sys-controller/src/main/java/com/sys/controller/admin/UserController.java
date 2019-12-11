58b451ec9953fc6b64e4bd562268fb2ed2c174d4
qiufeng001 <2227052723@qq.com>
2019-12-11 18:25:05 +0800

    测试系统是否能够执行

--- com-sys-controller/src/main/java/com/sys/controller/admin/UserController.java
+++ com-sys-controller/src/main/java/com/sys/controller/admin/UserController.java
@@ -6,6 +6,8 @@ import com.sys.model.admin.User;
 import com.sys.service.admin.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * UserController
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Controller;
  * @date 2019/11/1
  */
 @Controller
+@RequestMapping("/user/*")
 public class UserController extends BaseController<User, String> {
 
     @Autowired
@@ -29,5 +32,9 @@ public class UserController extends BaseController<User, String> {
         return "/admin/user";
     }
 
-
+    @RequestMapping("test")
+    @ResponseBody
+    public String test() {
+        return "test:";
+    }
 }
